7ba3b6289d0a0ed4432b789f9070376e4115f62f
andrey <an.sirvacheff@yandex.ru>
2025-01-11 16:05:16 +0500

    feat: add code for changing the first symbol in txt files

---
+++ change_files.py
@@ -0,0 +1,25 @@
+import os
+
+directory_path = '/home/andrey/PycharmProjects/Tank_AI/dataset/T-90/labels'
+
+files = [f for f in os.listdir(directory_path) if
+         os.path.isfile(os.path.join(directory_path, f)) and f.endswith('.txt')]
+
+print(files)
+
+for file_name in files:
+    file_path = os.path.join(directory_path, file_name)
+
+    with open(file_path, mode='r+') as f:
+        lines = f.readlines()
+
+        f.seek(0)
+
+        f.truncate()
+
+        for line in lines:
+            if len(line) > 0:
+                modified_line = '4' + line[1:]
+                f.write(modified_line)
+            else:
+                f.write(line)
